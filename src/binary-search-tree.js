ac651cd30c35dad830a9e8786de38554d74382df
Vitaly <vitalyasatka@gmail.com>
2023-04-18 04:33:27 +0500

    feat: add solutions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,218 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.rootItem = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootItem;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.rootItem === null) {
+      this.rootItem = new Node(data);
+    } else {
+      let current = this.root();
+      while (true) {
+        if (current.data > data) {
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(data);
+            break;
+          }
+        } else if (current.data < data) {
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(data);
+            break;
+          }
+        } else {
+          break;
+        }
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let current = this.root();
+    while (true) {
+      if (current.data > data) {
+        if (current.left) {
+          current = current.left;
+        } else {
+          return false;
+        }
+      } else if (current.data < data) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let current = this.root();
+    while (true) {
+      if (current.data > data) {
+        if (current.left) {
+          current = current.left;
+        } else {
+          return null;
+        }
+      } else if (current.data < data) {
+        if (current.right) {
+          current = current.right;
+        } else {
+          return null;
+        }
+      } else {
+        return current;
+      }
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    let current = this.root();
+    while (true) {
+      console.log(current);
+      if (current.data > data) {
+        if (current.left && current.left.data === data) {
+          let newCurrent = current.left;
+          console.log(current.left);
+          if (newCurrent.left && newCurrent.right) {
+            let minFromRight = newCurrent.right;
+            while (minFromRight.left) {
+              minFromRight = minFromRight.left;
+            }
+            newCurrent.data = minFromRight.data;
+            if (minFromRight.right) {
+              minFromRight = minFromRight.right;
+            } else {
+              minFromRight = null;
+            }
+          } else if (newCurrent.left) {
+            newCurrent.data = newCurrent.left.data;
+            newCurrent.left = newCurrent.left.left;
+            newCurrent.right = newCurrent.left.right;
+          } else if (newCurrent.right) {
+            newCurrent.data = newCurrent.right.data;
+            newCurrent.left = newCurrent.right.left;
+            newCurrent.right = newCurrent.right.right;
+          } else {
+            current.left = null;
+          }
+          console.log(current.left);
+          break;
+        } else {
+          if (current.left) {
+            current = current.left;
+          } else {
+            break;
+          }
+        }
+      } else if (current.data < data) {
+        if (current.right && current.right.data === data) {
+          let newCurrent = current.right;
+          console.log(current.right);
+          if (newCurrent.left && newCurrent.right) {
+            let minFromRight = newCurrent.right;
+            while (minFromRight.left) {
+              minFromRight = minFromRight.left;
+            }
+            newCurrent.data = minFromRight.data;
+            if (minFromRight.right) {
+              minFromRight = minFromRight.right;
+            } else {
+              minFromRight = null;
+            }
+          } else if (newCurrent.left) {
+            newCurrent.data = newCurrent.left.data;
+            newCurrent.left = newCurrent.left.left;
+            newCurrent.right = newCurrent.left.right;
+          } else if (newCurrent.right) {
+            newCurrent.data = newCurrent.right.data;
+            newCurrent.left = newCurrent.right.left;
+            newCurrent.right = newCurrent.right.right;
+          } else {
+            current.right = null;
+          }
+          console.log(current.right);
+          break;
+        } else {
+          if (current.right) {
+            current = current.right;
+          } else {
+            break;
+          }
+        }
+      } else {
+        console.log(current);
+        if (current.left && current.right) {
+          let minFromRight = current.right;
+          while (minFromRight.left) {
+            minFromRight = minFromRight.left;
+          }
+          current.data = minFromRight.data;
+          if (minFromRight.right) {
+            minFromRight = minFromRight.right;
+          } else {
+            minFromRight = null;
+          }
+        } else if (current.left) {
+          current.data = current.left.data;
+          current.left = current.left.left;
+          current.right = current.left.right;
+        } else if (current.right) {
+          current.data = current.right.data;
+          current.left = current.right.left;
+          current.right = current.right.right;
+        } else {
+          current.left = null;
+        }
+        console.log(current);
+        break;
+      }
+    }
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.root();
+
+    while (true) {
+      if (current.left) {
+        current = current.left;
+      } else {
+        return current.data;
+      }
+    }
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.root();
+
+    while (true) {
+      if (current.right) {
+        current = current.right;
+      } else {
+        return current.data;
+      }
+    }
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.data = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.data;
+  }
+
+  enqueue(value) {
+    if (this.data === null) {
+      this.data = new ListNode(value);
+      return;
+    }
+
+    let current = this.data;
+
+    while (true) {
+      if (current.next === null) {
+        current.next = new ListNode(value);
+        break;
+      }
+      current = current.next;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    console.log(this.getUnderlyingList());
+    let value = this.data.value;
+    this.data = this.data.next;
+    console.log(this.getUnderlyingList());
+    return value;
   }
 }
 
